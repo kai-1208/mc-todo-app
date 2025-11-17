de9f1caeb5e58b7556ffad53c75023f3af47d024
kai-1208 <tkhski1208@gmail.com>
2025-11-17 13:59:09 +0900

    repositoryNameを"mc-todo-app"に変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-const repositoryName = "react-todo-app";
+const repositoryName = "mc-todo-app";
 
 // https://vite.dev/config/
 export default defineConfig({
